843cade3701e0bb01fe3a1459b8b11f8df371492
Tunahan Uysal <tunahan.uysal@brands-a.com>
2023-07-21 11:02:01 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿// Baglantı olusturma
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqpuri");
+
+
+//Baglantı aktiflestirme ve Kanal acma
+using IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+//Queue olusturma
+
+channel.QueueDeclare(queue: "examplequeue", exclusive: false); // Consumerda bir kuyruk publisherdaki ile birebir aynı yapılandırma tanımlanmalıdır!
+
+//Queue!dan mesaj okuma 
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(queue: "examplequeue", autoAck:false, consumer:consumer); // autoAck ozelligi false ise  otomatik mesajı silmez ve consumerdan onay bekler 
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruga gelen mesajın işlendigi deger 
+
+    //e.body:Kuyruktaki mesajın verisini bütünsel getirecek
+
+    //e.Body.Span veya e.Body.Toarray(): Kuyruktaki mesajın byte verisini getirecektir
+
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+
+    Console.WriteLine(message);
+
+    channel.BasicAck(e.DeliveryTag, multiple: false);// false verilirse sadece bu mesaj için onay verir.True ise budan onceki mesajlar için de onay verir.
+};
+
+
+
+
+Console.Read();
