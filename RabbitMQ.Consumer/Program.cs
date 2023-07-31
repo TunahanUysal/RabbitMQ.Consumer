e06e9d9c6681d153285d7a392f8d029853ceff30
Tunahan Uysal <tuysal729@gmail.com>
2023-07-31 13:05:45 +0300

    updated

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -2,6 +2,7 @@
 
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 ConnectionFactory factory = new();
@@ -65,6 +66,162 @@ consumer.Received += (sender, e) =>
 //};
 
 
+// interface bir sınıfın belirli ozelliklere veya davranıslara sahip olması için tanımlanan bir sözleşmedir.İnterface bir sablondur.MEtodları imzalrı zorunlu olur ama işlevselligi belirtilmez.
+
+CustomerManager customerManager = new CustomerManager(new SecondLegislation());
+
+customerManager.Add();
+
+class CustomerManager
+{
+    private readonly ILegislation _legislation;
+
+    public CustomerManager(ILegislation legislation)
+    {
+        _legislation = legislation;
+    }
+
+    public void Add()
+    {
+        _legislation.DoTransaction();
+    }
+}
 
 
-Console.Read();
+interface ILegislation
+{
+    void DoTransaction();
+}
+
+class FirstLegislation : ILegislation
+{
+    public void DoTransaction()
+    {
+        Console.WriteLine("Processed according to the first legislation.");
+        Console.Read();
+    }
+}
+
+class SecondLegislation : ILegislation
+{
+    public void DoTransaction()
+    {
+        Console.WriteLine("Processed according to the second legislation.");
+        Console.Read();
+    }
+}
+
+
+// Solid  Yazılımın surdurebilirligını saglamak için gerekli prensiplerdir.5 adet prensip
+
+// Single Responsibility Principle ==> Tek sorumluluk 
+
+// Open Closed Principle => uygulamalar yeni bir ozellik eklemeye acık ama bu eklemeleri mevcut kodları degistirerek yapmamam gerrekiyor.
+
+// Liskov'S Subsitituon Principle // Sırf birbirine benziyor diye aynı catı altında tutmuyoruz musteri gercek musteri tuzel musteri isim soyisim ornegi
+
+// İnterface Seggregiration Principle  => interfacelerin dogru parcalara ayrılmasıdır.
+
+interface IBankConnector
+{
+    void Operation1();
+}
+
+interface IPrivateBankConnector : IBankConnector
+{
+    void Operation2();
+}
+
+interface IPublicBankConnector : IBankConnector
+{
+    void Operation3();
+}
+
+class CBank : IPrivateBankConnector
+{
+    public void Operation1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Operation2()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+// Dependency Inversion Principle => Yuksek seviyeleri sınıfların dusuk seviyeleri sınıfları somut degil de soyut halleri ile kullanmasıdır.
+
+//DependencInjection tasarım deseni kullanılır dependency inversion principle de
+
+
+//IOc container inversion of control  Ninject,Structure Map
+
+//AddSingleton,AddScoped,AddTransient  singleton uygulama yasamı boyunca tek br ornegi olusturulur scoped her http isteginde ornegi olusturuu.transient 
+
+// Data=>Business=>Services(Web Api(Json),WCF(Soap(Xml)))=>UI
+
+// Abstract Class
+
+public abstract class LegislationBase
+{
+    public int Id { get; set; }
+
+    protected LegislationBase()
+    {
+        Id= 0;
+        
+    }
+    public abstract void Evaluate();
+
+    public virtual void Save()
+    {
+        Console.WriteLine("Saved");
+
+    }
+}
+
+class ALegislation : LegislationBase
+{
+    public override void Evaluate()
+    {
+        Console.WriteLine("Assessed according to the alegislation.");
+    }
+}
+
+class BLegislation : LegislationBase
+{
+    public override void Evaluate()
+    {
+        Console.WriteLine("Assessed according to the blegislation.");
+    }
+}
+// virtual metodlar
+
+class CLegislation : LegislationBase
+{
+    public override void Evaluate()
+    {
+        Console.WriteLine("Assessed according to the blegislation.");
+    }
+
+    public override void Save()
+    {
+        //  base.Save();
+
+        Console.WriteLine("Saved according to the clegislation.");
+    }
+}
+
+//Access Modifier => Bir classın erşim bildirgecleri default olarak internaldır.
+
+//internal aynı namespace içinde aynı uygulama içinde kullanılabilir
+
+//public farklı assemblylerde de kullanılabilir farklı namespacelerde
+
+//private sadece kendini kapsayan suslu parantezler içerisinde gecerlidir  private<protected<internal<public
+
+//protected ise privateden artı olarak inheritence verdigi yerde de gecerlı olacaktır.
+// MVC arayuz için bir tasarım desenidir
+
+//Polymorphisim  =é  çok biçimlilikdir bir sınıfın farklı biçimdeki implemetasyonları kullanabilmesidir.
